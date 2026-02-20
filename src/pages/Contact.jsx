ddcf49243a2b53631ad7c8ceb602a94cfd1d3b0d
Lochini <luhradik1@gmail.com>
2026-02-21 01:03:32 +0530

    test webhook

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,19 +2,7 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>
-        If you have any questions or feedback, feel free to reach out.
-      </p>
-      <p>
-        📧 Email: contact@example.com
-      </p>
-      <p>
-        📞 Phone: +94 77 123 4567
-      </p>
-    </div>
-
+    <div>Contact</div>
   )
 }
 
